55c21e6b4a3f66e062f5131e39fbe884384c8602
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-17 08:16:36 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const result = record.find(superbowl => superbowl.result === "W")
+  return result ? result.year : undefined
+}
